dfadcd2172879f02417484360a699def4a9bddf7
Ankit Sharma <hsankit1999@gmail.com>
2017-12-02 22:18:43 +0530

    1

--- server/server.js
+++ server/server.js
@@ -2,7 +2,9 @@ const express=require('express');
 const path=require('path');
 const socketIO=require('socket.io');
 var http=require('http');
-
+var os=require('os');
+var bodyParser=require('body-parser');
+var coors=require('cors');
 const publicPath=path.join(__dirname,"../public");
 
 var app=express();
@@ -12,10 +14,15 @@ var port=process.env.PORT || 3000;
 var io=socketIO(server);
 var {generateMsg}=require('./message');
 app.use(express.static(publicPath));
+app.use(bodyParser.json());
+app.use(coors());
+app.get('/api',(req,res)=>{
+    res.json({'ms':req});
+});
 
 io.on('connection',(socket)=>{
     console.log("User added");
-
+    //console.log(http);
 
 
     socket.emit('newMessage',generateMsg("Admin","welcome in chat app"));
